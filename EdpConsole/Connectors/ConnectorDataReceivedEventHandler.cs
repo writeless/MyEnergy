1c6a98db9ec2a740ae09817a155bd6f8d049b674
Ricardo Carvalho <uiahhh@gmail.com>
2019-06-14 01:06:37 +0100

    remove received data event

--- EdpConsole/Connectors/ConnectorDataReceivedEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using EdpConsole.Core;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EdpConsole.Connectors
-{
-    public delegate void ConnectorDataReceivedEventHandler(IConnector sender, ModbusResponse response);
-}
